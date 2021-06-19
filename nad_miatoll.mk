a5483685354920831a55cee57b2d9ed4c63d7746
Angga <angga@linuxmail.org>
2021-06-20 02:10:12 +0700

    miatoll: lineage > nad
    Signed-off-by: Angga <angga@linuxmail.org>

--- nad_miatoll.mk
+++ nad_miatoll.mk
@@ -4,10 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_miatoll
+# Inherit some common nusantara stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official
+NAD_BUILD_TYPE := OFFICIAL
+
+# Device identifier
+PRODUCT_NAME := nad_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
